0c579e5e805e68afa706596deb67f50c64a4172a
An <15901037927@163.com>
2020-12-17 17:49:50 +0800

    update file

--- Go-project/函数.go
+++ Go-project/函数.go
@@ -1,5 +1,28 @@
 package main
 
+import "fmt"
+
+func aaa(a, b int, op string) (int, error) {
+	switch op {
+	case "+" :
+		return a + b, nil
+	case "-" :
+		return a - b, nil
+	case "*" :
+		return a * b, nil
+	case "/" :
+		return a / b, nil
+	default:
+		return 0, fmt.Errorf(
+		op)
+	}
+}
+
 func main() {
+	if resout, err := aaa(3, 4, "99"); err !=nil{
+		fmt.Println("erroe: ", err)
+	}else {
+		fmt.Println(resout)
+	}
 
 }
