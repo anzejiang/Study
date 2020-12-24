93aaafd69d6873d608b871d3f07dd12bfcbd284f
An <15901037927@163.com>
2020-12-24 17:24:15 +0800

    update file

--- Go-project/retriever/main.go
+++ Go-project/retriever/main.go
@@ -1,10 +1,20 @@
 package main
 
-func download(r Retiever) string  {
-	return r.get("www.imooc.com")
+import (
+	"fmt"
+	"go-Study/Go-project/retriever/mockretriever"
+)
 
+type Retriever interface {
+	Get(url string) string
+}
+
+func download(r Retriever) string  {
+	return r.Get("www.imooc.com")
 }
 
 func main() {
-	
+	var r Retriever
+	r = mock.Retriever{"The is a fake Content"}
+	fmt.Println(download(r))
 }
