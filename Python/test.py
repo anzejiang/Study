21e75a9ec3182d10ed32962d1fbaf9134baf7433
ZeJang An <15901037927@163.com>
2022-03-10 18:35:45 +0800

    create test file

---
+++ Python/test.py
@@ -0,0 +1,13 @@
+
+def test(a):
+    for i in a:
+        if i == 1:
+            continue
+        else:
+            print(i)
+
+
+if __name__ == '__main__':
+    print("The is a Test Python File.")
+    a = [1, 2, 3, 4, 5]
+    test(a)
